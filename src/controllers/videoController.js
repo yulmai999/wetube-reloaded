dd73402c66613bad05313da39e58ca236549db7e
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2022-08-20 06:24:35 +0900

    8/20
    backend start, watch.pug ing

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,30 +1,44 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: true,
-};
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 
 export const tranding = (req, res) => {
-  const videos = [
-    {
-      title: "Hello",
-    },
-    {
-      title: "Video #2",
-    },
-    {
-      title: "Whatsup",
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  return res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.send("Edit");
 
 export const upload = (req, res) => res.send("upload");
 
+export const search = (req, res) => res.send("Search");
+
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   return res.send("delete");
 };
